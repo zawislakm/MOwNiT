a4101c05c24436a99156c3444d82697ae075f296
Maks <mzawislak@student.agh.edu.pl>
2023-03-25 16:11:01 +0100

    stealing done

--- Lab4/MOwNiT_lab4.py
+++ Lab4/MOwNiT_lab4.py
@@ -42,49 +42,47 @@ def sqrdiffrence(X, interpoleted, n):
     return ans
 
 
-def cubicxd(X, nodes, n, type):
-    def hi(i):
-        h = nodes[i] - nodes[i - 1]  # dif between nodes
-        return h
+def hi(i, nodes):
+    h = nodes[i] - nodes[i - 1]  # dif between nodes
+    return h
 
-    def delta(i):
-        d = (f(nodes[i]) - f(nodes[i - 1])) / hi(i)
-        return d
 
+def delta(i, nodes):
+    d = (f(nodes[i]) - f(nodes[i - 1])) / hi(i, nodes)
+    return d
+
+
+def cubic_spline(X, nodes, n, type):
     matrix = [[0 for _ in range(n)] for _ in range(n)]
     vector = [0 for _ in range(n)]
 
     for i in range(1, n - 1):  # 1 brzegi3
-        matrix[i][i - 1] = hi(i)
-        matrix[i][i] = 2 * (hi(i) + hi(i + 1))
-        matrix[i][i + 1] = hi(i + 1)
-        vector[i] = delta(i + 1) - delta(i)
+        matrix[i][i - 1] = hi(i, nodes)
+        matrix[i][i] = 2 * (hi(i, nodes) + hi(i + 1, nodes))
+        matrix[i][i + 1] = hi(i + 1, nodes)
+        vector[i] = delta(i + 1, nodes) - delta(i, nodes)
 
+
+    #change types
     if type == 0:
         matrix[0][0] = 2
         matrix[0][1] = 1
+        matrix[n - 1][n - 2] = 2
+        matrix[n - 1][n - 1] = 1
+        vector[0] = 0
+        vector[n - 1] = 0
     else:
         matrix[0][0] = 1
         matrix[0][1] = 0
-    if type == 0:
-        matrix[n - 1][n - 2] = 2
-        matrix[n - 1][n - 1] = 1
-    else:
         matrix[n - 1][n - 2] = 0
         matrix[n - 1][n - 1] = 1
-    if type == 0:
         vector[0] = 0
-    else:
-        vector[0] = 0
-    if type == 0:
-        vector[n - 1] = 0
-    else:
         vector[n - 1] = 0
 
     ROs = np.linalg.solve(matrix, vector)
 
     def bi(i):
-        h = hi(i)
+        h = hi(i, nodes)
         y_next = f(nodes[i])
         y = f(nodes[i - 1])
         ro_next = ROs[i]
@@ -98,7 +96,7 @@ def cubicxd(X, nodes, n, type):
         return c
 
     def di(i):
-        h = hi(i)
+        h = hi(i, nodes)
         ro_next = ROs[i]
         ro = ROs[i - 1]
         d = (ro_next - ro) / h
@@ -119,107 +117,44 @@ def cubicxd(X, nodes, n, type):
     return ans
 
 
-def quadratic(X, nodes, n, type):
-    def hi(i):
-        h = nodes[i] - nodes[i - 1]  # dif between nodes
-        return h
-
-    def delta(i):
-        d = (f(nodes[i]) - f(nodes[i - 1])) / hi(i)
-        return d
-
+def quadratic_spline(X, nodes, n, mode):
     matrix = [[0 for _ in range(n)] for _ in range(n)]
     vector = [0 for _ in range(n)]
 
-    for i in range(1, n - 1):
-        # prep
-        d = delta(i)
-        # matrix
+    for i in range(1, n - 1):  # 1 brzegi2
         matrix[i][i - 1] = 1
         matrix[i][i] = 1
-        # vector
-        vector[i] = 2 * d
-    vector[n - 1] = 2 * delta(n - 1)
+        vector[i] = 2 * delta(i, nodes)
+    vector[n - 1] = 2 * delta(n - 1, nodes)
 
-    if type:
-        pass
-    else:
-        pass
-
-    GAs = np.linalg.solve(matrix, vector)
-
-    def bi(i):
-        b = GAs[i - 1]
-        return b
-
-    def ci(i):
-        h = hi(i)
-        GA_next = GAs[i + 1]
-        GA = GAs[i]
-        c = (GA_next - GA) / (2 * h)
-        return c
-
-    ans = [0 for _ in range(n)]
-    for j in range(len(X)):
-        i = 0
-        while i < len(nodes) - 1 and nodes[i] < X[j]:
-            i += 1
-        y = f(nodes[i - 1])
-        b = bi(i)
-        c = ci(i)
-        dif = (X[j - 1] - nodes[i - 1])
-        ans[j] = y + b * dif + c * dif ** 2
-    return ans
-
-
-def qxd(X, nodes, n, mode):
-    y = f(nodes)
-    nodes_n = len(nodes)
-
-    def hi(i):
-        return nodes[i] - nodes[i - 1]
-
-    def delta(i):
-        return (y[i] - y[i - 1]) / (nodes[i] - nodes[i - 1])
-
-    GAs = []
-
-    matrix = [[0 for _ in range(nodes_n)] for _ in range(nodes_n)]
-    vector = [0 for _ in range(nodes_n)]
-
-    for i in range(1, nodes_n - 1):  # 1 brzegi2
-        matrix[i][i - 1] = 1
-        matrix[i][i] = 1
-        vector[i] = 2 * delta(i)
-    vector[n - 1] = 2 * delta(n - 1)
-
+    #change types
     if mode == 0:
         matrix[0][0] = 1
         matrix[0][1] = 0
+        vector[0] = delta(1, nodes)
     else:
         matrix[0][0] = 1
         matrix[0][1] = 0
-    matrix[nodes_n - 1][nodes_n - 2] = 1
-    matrix[nodes_n - 1][nodes_n - 1] = 1
-    if mode == 0:
-        vector[0] = delta(1)
-    else:
         vector[0] = 0
 
-    GAs = np.linalg.solve(matrix, vector)
+    matrix[n - 1][n - 2] = 1
+    matrix[n - 1][n - 1] = 1
+
+    ROs = np.linalg.solve(matrix, vector)
 
     def bi(i):
-        b = GAs[i - 1]
+        b = ROs[i - 1]
         return b
 
     def ci(i):
         b2_next = bi(i + 1)
         b2 = bi(i)
-        h = hi(i)
+        h = hi(i, nodes)
         c = (b2_next - b2) / (2 * h)
         return c
 
     ans = [0 for _ in range(len(X))]
+
     for j in range(len(X)):
         i = 0
         while i < len(nodes) - 1 and nodes[i] < X[j]:
@@ -237,4 +172,5 @@ cheby_nodes = chebyshev_disrtibution(n, min_x, max_x)
 parallel_nodess = np.arange(min_x, max_x + 0.01, (max_x - min_x) / (n - 1))
 X = np.arange(min_x, max_x + 0.01, 0.01)
 
-cubicxd(X, cheby_nodes, n, 0)
+cubic_spline(X, cheby_nodes, n, 0)
+quadratic_spline(X, cheby_nodes, n, 0)
